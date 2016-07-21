c2144ece849f362a1abfc2ee5aa608d776d0a1b4
Alex <catharsis96@yandex.ru>
2016-07-22 00:27:42 +0300

    fixed

--- serial2007.py
+++ serial2007.py
@@ -1,4 +1,4 @@
-#import serial
+import serial
 import sys
 from time import sleep
 import json
@@ -7,8 +7,7 @@ import re
 import requests
 
 try:
-  ser = serial.Serial("COM6", 115200,timeout=0, parity=serial.PARITY_NONE, 
-                        stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+  ser = serial.Serial("COM6", 115200,timeout=0, parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 except:
   sys.exit("Error connecting device")
 
